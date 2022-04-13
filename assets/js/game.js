c8709e122892d9b0a3f587fd4f5005fc5a77d947
poofy <skyang014@gmail.com>
2022-04-12 20:12:18 -0700

    shop funtion

--- assets/js/game.js
+++ assets/js/game.js
@@ -100,8 +100,56 @@ var endGame = function () {
         }
 };
 
-//function start a new game
+//shop
+var shop = function () {
+    //ask player what they'd like to do
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, of LEAVE the store?  Please enter one: REFILL, UPGRADE, or LEAVE to make a choice."
+    );
+    //switch option
+    switch (shopOptionPrompt) {
+        case "REFILL": //new case
+        case "refill":
+            if (playerMoney > 7) {
+                window.alert("Refilling player's health by 20 for 7 dollars.");
+                //increase health and decrease money
+                playerHealth = playerHealth + 20;
+                playerMoney = playerMoney - 7;
+                }
+            else {
+                window.alert("You don't have enough money!");
+            }
+            break;
+        
+        case "UPGRADE": //new case
+        case "upgrade":
+            if (playerMoney > 7) {
+                window.alert ("Upgraing player's attack by 6 for 7 dollars.");
+                //increase attack and decrease money
+                playerAttack = playerAttack + 6;
+                playerMoney = playerMoney - 7;
+            }
+            else {
+                window.alert("You don't have enough money!");
+            }
+            break;
+        
+        case "LEAVE":
+        case "leave":
+            window.alert("Leaving the store.");
+            //do nothing so fn will end
+            break;
+        
+        default:
+            window.alert("You did not pick a valid option. Try again.");
+            //call shop again to pick a valid item
+            shop();
+            break;
 
+    }
+};
+
+//function start a new game
 var startGame = function() {
     //reset player stats
     playerHealth = 100;
@@ -120,6 +168,15 @@ var startGame = function() {
             // debugger;
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
             fight(pickedEnemyName);
+            //if we're not at the last enemy in the array
+            if (playerHealth > 0 && i < enemyNames.length - 1) {
+                //ask if player wants to use the store before next round
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?")
+                    //if yes, take them to the store function
+                    if (storeConfirm) {
+                    shop();
+                    }
+            }
         }
         else {
             window.alert("You have lost your robot in battle! Game Over!");
